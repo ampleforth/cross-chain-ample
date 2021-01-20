86a31d0e63340e1ea4ab1e1aa2423bc7c78dc805
nithin <nithin@fragments.org>
2021-01-20 13:18:27 -0800

    updated mocks

--- contracts/_mocks/MockAmpleforth.sol
+++ contracts/_mocks/MockAmpleforth.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.6.12;
 
-contract MockAmplPolicy {
+contract MockAmpleforth {
     uint256 public epoch;
 
     function updateEpoch(uint256 epoch_) external {
